737a8814c380ca2e25ed6855d4eb5ad24a115b92
golden-dawn <constantin_m_adam@yahoo.com>
2018-02-04 03:43:49 -0500

    Cleaned up data types; changed column order

--- db_api/app/models/db_models.py
+++ db_api/app/models/db_models.py
@@ -5,10 +5,10 @@ db = SQLAlchemy()
 
 class Equity(db.Model):
     __tablename__ = 'equities'
-    ticker = db.Column(db.String(), primary_key=True)
-    name = db.Column(db.String())
-    family = db.Column(db.String())
-    exchange = db.Column(db.String(), db.ForeignKey('exchanges.name'),
+    ticker = db.Column(db.String(8), primary_key=True)
+    name = db.Column(db.String(64))
+    family = db.Column(db.String(64))
+    exchange = db.Column(db.String(16), db.ForeignKey('exchanges.name'),
                          index=True)
     eod_data = db.relationship('EOD', backref='equities', lazy=True)
     id_data = db.relationship('Intraday', backref='equities', lazy=True)
@@ -19,69 +19,69 @@ class Equity(db.Model):
 
 class Exchange(db.Model):
     __tablename__ = 'exchanges'
-    name = db.Column(db.String(), primary_key=True)
+    name = db.Column(db.String(16), primary_key=True)
     holidays = db.relationship('Holiday', backref='exchanges', lazy=True)
 
 
 class EOD(db.Model):
     __tablename__ = 'eods'
-    date = db.Column(db.Date, primary_key=True)
-    stk = db.Column(db.String(), db.ForeignKey('equities.ticker'),
+    stk = db.Column(db.String(8), db.ForeignKey('equities.ticker'),
                     primary_key=True)
-    o = db.Column(db.Numeric())
-    hi = db.Column(db.Numeric())
-    lo = db.Column(db.Numeric())
-    c = db.Column(db.Numeric())
+    date = db.Column(db.Date, primary_key=True)
+    o = db.Column(db.Numeric(10, 2))
+    hi = db.Column(db.Numeric(10, 2))
+    lo = db.Column(db.Numeric(10, 2))
+    c = db.Column(db.Numeric(10, 2))
     volume = db.Column(db.Integer)
     open_interest = db.Column(db.Integer)
 
 
 class Intraday(db.Model):
     __tablename__ = 'intradays'
-    datetime = db.Column(db.DateTime, primary_key=True)
     stk = db.Column(db.String(), db.ForeignKey('equities.ticker'),
                     primary_key=True)
-    o = db.Column(db.Numeric())
-    hi = db.Column(db.Numeric())
-    lo = db.Column(db.Numeric())
-    c = db.Column(db.Numeric())
+    datetime = db.Column(db.DateTime, primary_key=True)
+    o = db.Column(db.Numeric(10, 2))
+    hi = db.Column(db.Numeric(10, 2))
+    lo = db.Column(db.Numeric(10, 2))
+    c = db.Column(db.Numeric(10, 2))
     volume = db.Column(db.Integer)
     open_interest = db.Column(db.Integer)
 
 
 class Option(db.Model):
     __tablename__ = 'options'
-    date = db.Column(db.Date, primary_key=True)
-    und = db.Column(db.String(), db.ForeignKey('equities.ticker'),
-                    primary_key=True)
     expiry = db.Column(db.Date, primary_key=True)
+    und = db.Column(db.String(8), db.ForeignKey('equities.ticker'),
+                    primary_key=True)
     cp = db.Column(db.String(1), primary_key=True)
-    strike = db.Column(db.Numeric(), primary_key=True)
-    bid = db.Column(db.Numeric())
-    ask = db.Column(db.Numeric())
+    strike = db.Column(db.Numeric(10, 2), primary_key=True)
+    date = db.Column(db.Date, primary_key=True)
+    bid = db.Column(db.Numeric(10, 2))
+    ask = db.Column(db.Numeric(10, 2))
     volume = db.Column(db.Integer)
 
 
 class OptSpot(db.Model):
     __tablename__ = 'opt_spots'
-    date = db.Column(db.Date, primary_key=True)
-    stk = db.Column(db.String(), db.ForeignKey('equities.ticker'),
+    stk = db.Column(db.String(8), db.ForeignKey('equities.ticker'),
                     primary_key=True)
-    spot = db.Column(db.Numeric())
+    date = db.Column(db.Date, primary_key=True)
+    spot = db.Column(db.Numeric(10, 2))
 
 
 class Dividend(db.Model):
     __tablename__ = 'dividends'
-    date = db.Column(db.Date, primary_key=True)
-    stk = db.Column(db.String(), db.ForeignKey('equities.ticker'),
+    stk = db.Column(db.String(8), db.ForeignKey('equities.ticker'),
                     primary_key=True)
-    ratio = db.Column(db.Float)
+    date = db.Column(db.Date, primary_key=True)
+    ratio = db.Column(db.Numeric(10, 4))
     divi_type = db.Column(db.SmallInteger)
 
 
 class Holiday(db.Model):
     __tablename__ = 'holidays'
     date = db.Column(db.Date, primary_key=True)
-    exchange = db.Column(db.String(), db.ForeignKey('exchanges.name'),
+    exchange = db.Column(db.String(16), db.ForeignKey('exchanges.name'),
                          primary_key=True)
-    name = db.Column(db.String())
+    name = db.Column(db.String(64))
